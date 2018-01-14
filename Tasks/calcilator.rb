ef2060b0baf6aa6a9266d5b264d28ab2257aeff0
Kirilloid <kirill.the.spike@gmail.com>
2018-01-14 23:13:33 +0300

    Add first task (calc)

---
+++ Tasks/calcilator.rb
@@ -0,0 +1,86 @@
+#Калькулятор
+#Весьма самодекларируемый код функций 
+def Plus(int1, int2)
+	answer = int1.to_i + int2.to_i
+	return answer
+end
+
+def Minus(int1, int2)
+	return int1.to_i - int2.to_i
+end
+
+def Multiplication(int1, int2)
+	return int1.to_i * int2.to_i
+end
+
+def Division(int1, int2)
+	if int2.to_i == 0
+		return "Нельзя делить на тебя"
+	end
+	return int1.to_i/int2.to_i
+end
+
+
+# def Choise(int1, int2)
+# 	puts "1- +  3- * \n2- -  4- /"
+# 	number = gets
+# 	case number
+# 	when 1
+# 		answer = Plus(int1, int2)
+# 		puts "Аааатветик: #{answer}"
+# 	end
+# 	when 2
+# 		answer = Minus(int1, int2)
+# 		puts "Результатик: #{answer}"
+# 	when 3
+# 		answer = Multiplication(int1, int2)
+# 		puts "Умножил: #{answer}"
+# 	when 4
+# 		answer = Division(int1, int2)
+# 		puts "Что тут у нас?: #{answer}"
+# 	when String 
+# 		puts "Невозможный вариант, но пусть будет"
+# 	else 
+# 		puts "От 1 до 4, овощ!"
+# 	end
+# end
+
+#Магия начинается тут!!
+
+puts "Вазап, таких калькуляторов ты ещё не видел!"
+
+begin
+
+puts "Вводи сначала первое, потом второе число:"
+
+int1 = gets
+int2 = gets
+
+puts "А теперь выбирай что с ними сделать:"
+
+puts "1- +  3- * \n2- -  4- /"
+	number = gets
+	case number.to_i
+	when 1
+		answer = Plus(int1, int2)
+		puts "Аааатветик: #{answer}"
+	when 2
+		answer = Minus(int1, int2)
+		puts "Результатик: #{answer}"
+	when 3
+		answer = Multiplication(int1, int2)
+		puts "Умножил: #{answer}"
+	when 4
+		answer = Division(int1, int2)
+		puts "Что тут у нас?: #{answer}"
+	when String 
+		puts "Невозможный вариант, но пусть будет"
+	else 
+		puts "От 1 до 4, овощ!"
+	end
+
+puts 'Ещё разок? "Yes" если понравилось...'
+omt = gets
+omt.downcase!
+omt.chomp!
+end while omt == "yes"
