de854ccf5645a0986e48ec06fdf7f8c02d08d934
Kirilloid <kirill.the.spike@gmail.com>
2018-01-15 16:59:23 +0300

    vse pofiksil

--- tasks/DCP_calc_done.rb
+++ tasks/DCP_calc_done.rb
@@ -4,20 +4,16 @@ def plus(int1, int2)
   answer = int1.to_i + int2.to_i
   answer
 end
-
 def minus(int1, int2)
   int1.to_i - int2.to_i
 end
-
 def multiplication(int1, int2)
   int1.to_i * int2.to_i
 end
-
 def division(int1, int2)
   return 'Нельзя делить на тебя' if int2.to_i.zero?
   int1.to_i / int2.to_i
 end
-
 def choise(number, int1, int2)
   case number.to_i
   when 1
@@ -46,9 +42,9 @@ loop do
   puts "1- +  3- * \n2- -  4- /"
   number = gets
   choise(number, int1, int2)
-  puts 'Ещё разок? "Yes" если понравилось...'
+  puts 'Ещё разок? "no" for exit...'
   omt = gets
   omt.downcase!
   omt.chomp!
-  break if omt == "yes"
+  break if omt == 'no'
 end
